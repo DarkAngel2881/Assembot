46fb31e97f70b5a952890da4d5450d1d411d6153
DarkAngel2881 <100215802+DarkAngel2881@users.noreply.github.com>
2022-11-29 20:41:37 +0100

    Update Responses.py

--- Responses.py
+++ Responses.py
@@ -1,19 +1,27 @@
+#functions for date needings
 from datetime import datetime
 
+#filters for the responses based on the user text message
 def sample_responses(input_text):
     user_message = str(input_text).lower()
 
     if user_message in ("hello", "hi", "what's up"):
-        
+
         return "Hey! How's it going?"
 
     if user_message in ("who are you", "who are you?"):
         
         return "You can call me AssemBot and I am the way you can manage a request for a class assembly!"
 
-    if user_message in ("what time is it?", "time?"):
+    if user_message in ("date?", "date", "what day is it", "what day is it?", "day", "day?"):
         now = datetime.now()
-        date_time = now.strftime("%d/%m/%y", "%h:%m:%s")
+        date_time = "Today is " + (now.strftime("%A %d of %B %Y"))
+
+        return str(date_time)
+    
+    if user_message in ("time?", "time", "what time is it", "what time is it?"):
+        now = datetime.now()
+        date_time = now.strftime("%H:%M:%S")
 
         return str(date_time)
 
